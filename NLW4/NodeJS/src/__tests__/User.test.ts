c4c6df2e69481a7918341990317f676607e36961
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2021-04-19 00:50:13 -0300

    Integration Test

--- NLW4/NodeJS/src/__tests__/User.test.ts
+++ NLW4/NodeJS/src/__tests__/User.test.ts
@@ -1,10 +1,31 @@
 import request from 'supertest';
 import { app } from '../app';
 
+import createConnection from '../database';
+
 describe("User", () => {
-  request(app).post('/users')
+  beforeAll(async () => {
+    const connection = await createConnection(); 
+    await connection.runMigrations();
+  });
+  
+  it("Should be able to create a new User", async () => {
+    const response = await request(app).post('/users')
+      .send({
+        email: 'example@email.com',
+        name: 'User Example'
+      });
+
+      expect(response.status).toBe(201);
+  });
+
+  it("Should not to be able to create a new user with an existent email", async () => {
+    const response = await request(app).post('/users')
     .send({
-      email: "user@example.com",
-      name: "User Example"
+      email: 'example@email.com',
+      name: 'User Example 2'
     })
+
+    expect(response.status).toBe(400);
+  });
 });
--- NLW4/NodeJS/src/app.ts
+++ NLW4/NodeJS/src/app.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
 import express from 'express';
-import "./database"
+import createConnection from "./database";
 import { router } from './routes';
+
+createConnection();
 
 const app = express();
 
