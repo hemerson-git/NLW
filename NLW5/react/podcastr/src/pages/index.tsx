78d6863f15ec696e428cf53b1668ac84a7f91505
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2021-06-01 00:06:54 -0300

    Production Build

--- NLW5/react/podcastr/src/pages/index.tsx
+++ NLW5/react/podcastr/src/pages/index.tsx
@@ -1,5 +1,21 @@
+import api from "../services/api";
+
 export default function Home() {
   return (
     <h1>index</h1>
   );
 };
+
+export async function getStaticProps() {
+  const response = await api.get('/episodes');
+  const { data } = await response;
+  console.log(data);
+  
+  return {
+    props: {
+      episodes: data,
+    },
+
+    revalidate: 60 * 60 * 8, // 8h
+  }
+}
