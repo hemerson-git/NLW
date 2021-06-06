4812e638adf625171a3a571a542daaa249c515ae
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2021-06-05 21:52:47 -0300

    Provider update

---
+++ NLW5/react/podcastr/src/contexts/PlayerContext.tsx
@@ -0,0 +1,57 @@
+import { createContext, useState } from "react";
+
+interface Episode {
+  title: string;
+  members: string;
+  thumbnail: string;
+  duration: number;
+  url: string;
+}
+
+interface PlayerContextData {
+  episodeList: Episode[];
+  currentEpisodeIndex: number;
+  isPlaying: boolean;
+  togglePlay: () => void;
+  play: (episode: Episode) => void;
+  setPlayingState: (state: boolean) => void;
+}
+
+const PlayerContext = createContext({} as PlayerContextData);
+
+export default PlayerContext;
+
+export function PlayerContextProvider({ children }) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function play(episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state);
+  }
+
+  return (
+    <PlayerContext.Provider
+      value={{
+        episodeList,
+        currentEpisodeIndex,
+        isPlaying,
+        play,
+        togglePlay,
+        setPlayingState,
+      }}
+    >
+      {children}
+    </PlayerContext.Provider>
+  );
+}
