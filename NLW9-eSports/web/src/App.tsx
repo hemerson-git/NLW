4abbe9bade60df01d19be61853ba0bb393d3d482
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2022-09-13 00:51:23 -0300

    PARTIAL Home Page

--- NLW9-eSports/web/src/App.tsx
+++ NLW9-eSports/web/src/App.tsx
@@ -1,11 +1,51 @@
 import { useState } from "react";
 
+import logoImg from "./assets/logo.svg";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="container min-h-screen min-w-full bg-purple-700 flex items-center justify-center">
-      <h1 className="text-lg text-white">Hello, World</h1>
+    <div className="max-w-[1344px] mx-auto flex items-center flex-col my-20">
+      <img src={logoImg} alt="" />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{" "}
+        <span className="bg-nlw-gradient bg-clip-text text-transparent">
+          duo
+        </span>{" "}
+        está aqui
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="" className="relative">
+          <img src="/game_1.jpg" alt="" className="rounded-md" />
+
+          <div className="w-full pt-16 pb-4 px-4 absolute text-white bottom-0">
+            <strong>League of Legends</strong>
+          </div>
+        </a>
+
+        <a href="">
+          <img src="/game_2.jpg" alt="" className="rounded-md" />
+        </a>
+
+        <a href="">
+          <img src="/game_3.jpg" alt="" className="rounded-md" />
+        </a>
+
+        <a href="">
+          <img src="/game_4.jpg" alt="" className="rounded-md" />
+        </a>
+
+        <a href="">
+          <img src="/game_5.jpg" alt="" className="rounded-md" />
+        </a>
+
+        <a href="">
+          <img src="/game_6.jpg" alt="" className="rounded-md" />
+        </a>
+      </div>
     </div>
   );
 }
--- NLW9-eSports/web/tailwind.config.cjs
+++ NLW9-eSports/web/tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/bg-galaxy.png')",
+        "nlw-gradient":
+          "linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%);",
+      },
+    },
   },
   plugins: [],
 };
