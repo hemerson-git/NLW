fbd01fcaa29ac9f482cecd3cee405c519b4009f4
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2022-09-14 13:18:36 -0300

    FINISH Home

---
+++ NLW9-eSports/mobile/src/components/GameCard/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  cover: {
+    width: 240,
+    height: 320,
+    justifyContent: "flex-end",
+    borderRadius: 8,
+    overflow: "hidden",
+  },
+
+  footer: {
+    width: "100%",
+    height: 120,
+    padding: 16,
+    justifyContent: "flex-end",
+  },
+});
--- NLW9-eSports/mobile/src/pages/Home/index.tsx
+++ NLW9-eSports/mobile/src/pages/Home/index.tsx
@@ -1,20 +1,44 @@
-import { Image, VStack } from "native-base";
+import { FlatList, Image, VStack } from "native-base";
 
-import { Background } from "../../components/Background";
 import logo from "../../assets/logo-nlw-esports.png";
+
+// COMPONENTS
+import { Background } from "../../components/Background";
 import { Heading } from "../../components/Heading";
+import { GameCard } from "../../components/GameCard";
+
+// UTILS
+import { GAMES } from "../../utils/games";
 
 export function Home() {
   return (
     <Background>
-      <VStack alignItems="center" justifyItems="center" pt={"24"} mb={"10"}>
-        <Image source={logo} alt="" width={240} h={135} />
+      <VStack alignItems="center" justifyItems="center" mt={74} mb={12}>
+        <Image source={logo} alt="" width={214} h={120} />
       </VStack>
 
       <Heading
         title="Encontre seu duo!"
         subtitle="Conecte-se e comece a jogar..."
         p={6}
+      />
+
+      <FlatList
+        data={GAMES}
+        horizontal
+        pl={8}
+        contentContainerStyle={{ paddingRight: 64 }}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <GameCard
+            data={{
+              ads: item.ads,
+              cover: item.cover,
+              name: item.name,
+              id: item.id,
+            }}
+          />
+        )}
       />
     </Background>
   );
