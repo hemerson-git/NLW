b1b5680e11eee54624c208894afadccd5c222bde
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2022-09-14 23:23:15 -0300

    ADD Database

--- NLW9-eSports/server/src/server.ts
+++ NLW9-eSports/server/src/server.ts
@@ -1,9 +1,20 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const PORT = 3000;
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(express.json());
+
+app.get("/games", async (req, res) => {
+  try {
+    const games = await prisma.game.findMany();
+    res.send(games);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.get("/", (req, resp) => {
   console.log("Hello, World!");
