ccfcb49f2eef770bd6b350dcf0e9bd10157e918e
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2022-11-01 23:57:08 -0300

    CREATE: seed, ADD: new models

---
+++ NLW10-Copa/server/prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: "John Doe",
+      email: "john.doe@gmail.com",
+      avatarUrl: "https://github.com/hemerson-dev.png",
+    },
+  });
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: "Example Pool",
+      code: "BOL123",
+      ownerId: user.id,
+
+      participants: {
+        create: {
+          userId: user.id,
+        },
+      },
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-11-02T12:00:00.201Z",
+      firstTeamCountryCode: "DE",
+      secondTeamCountryCode: "BR",
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-11-03T12:00:00.201Z",
+      firstTeamCountryCode: "BR",
+      secondTeamCountryCode: "AR",
+
+      guesses: {
+        create: {
+          firstTeamPoints: 2,
+          secondTeamPoints: 0,
+
+          participant: {
+            connect: {
+              userId_poolId: {
+                poolId: pool.id,
+                userId: user.id,
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+}
+
+main();
