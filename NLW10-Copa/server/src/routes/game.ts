791e05ad603386fed350077847e080fa0a432ce5
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2022-11-04 00:01:50 -0300

    CREATE: Guess, and Game routes

--- NLW10-Copa/server/src/routes/game.ts
+++ NLW10-Copa/server/src/routes/game.ts
@@ -1,5 +1,7 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
 import { prisma } from "../lib/prisma";
+import { authenticate } from "../plugins/authenticate";
 
 export async function gameRoutes(fastify: FastifyInstance) {
   fastify.get("/games/count", async () => {
@@ -7,4 +9,43 @@ export async function gameRoutes(fastify: FastifyInstance) {
 
     return { count };
   });
+
+  fastify.get(
+    "/polls/:id/games",
+    { onRequest: [authenticate] },
+    async (request, response) => {
+      const getPollParams = z.object({
+        id: z.string(),
+      });
+
+      const { id } = getPollParams.parse(request.params);
+
+      const games = await prisma.game.findMany({
+        orderBy: {
+          date: "desc",
+        },
+
+        include: {
+          guesses: {
+            where: {
+              participant: {
+                userId: request.user.sub,
+                pollId: id,
+              },
+            },
+          },
+        },
+      });
+
+      return {
+        games: games.map((game) => {
+          return {
+            ...game,
+            guess: game.guesses.length > 0 ? game.guesses[0] : null,
+            guesses: undefined,
+          };
+        }),
+      };
+    }
+  );
 }
--- NLW10-Copa/server/src/routes/guess.ts
+++ NLW10-Copa/server/src/routes/guess.ts
@@ -1,5 +1,7 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
 import { prisma } from "../lib/prisma";
+import { authenticate } from "../plugins/authenticate";
 
 export async function guessRoutes(fastify: FastifyInstance) {
   fastify.get("/guesses/count", async () => {
@@ -7,4 +9,78 @@ export async function guessRoutes(fastify: FastifyInstance) {
 
     return { count };
   });
+
+  fastify.post(
+    "/polls/:pollId/games/:gameId/guesses",
+    { onRequest: authenticate },
+    async (request, response) => {
+      const createGuessParams = z.object({
+        pollId: z.string(),
+        gameId: z.string(),
+      });
+
+      const createGuessBody = z.object({
+        firstTeamPoints: z.number(),
+        secondTeamPoints: z.number(),
+      });
+
+      const { gameId, pollId } = createGuessParams.parse(request.params);
+      const { firstTeamPoints, secondTeamPoints } = createGuessBody.parse(
+        request.body
+      );
+
+      const participant = await prisma.participant.findUnique({
+        where: {
+          userId_pollId: {
+            pollId,
+            userId: request.user.sub,
+          },
+        },
+      });
+
+      if (!participant) {
+        return response.status(400).send({
+          message: "You're not allowed to create guesses inside this poll",
+        });
+      }
+
+      const guess = await prisma.guess.findUnique({
+        where: {
+          participantId_gameId: {
+            participantId: participant.id,
+            gameId,
+          },
+        },
+      });
+
+      if (guess)
+        return response
+          .status(400)
+          .send("You have already guessed in this game on this poll");
+
+      const game = await prisma.game.findUnique({
+        where: {
+          id: gameId,
+        },
+      });
+
+      if (!game)
+        return response.status(404).send({ message: "Game not found" });
+
+      if (game.date < new Date()) {
+        return response.status(400).send({
+          message: "You can't send guesses after de end of the game",
+        });
+      }
+
+      await prisma.guess.create({
+        data: {
+          gameId,
+          participantId: participant.id,
+          firstTeamPoints,
+          secondTeamPoints,
+        },
+      });
+    }
+  );
 }
