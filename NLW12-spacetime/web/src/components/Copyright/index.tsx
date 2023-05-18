1c72e17755ad16c115e2660a96a25767ae66a28c
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2023-05-18 20:59:37 -0300

    ADD: Github oauth

---
+++ NLW12-spacetime/web/src/components/Copyright/index.tsx
@@ -0,0 +1,26 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      <span>
+        Feito com 💜 no NLW da{' '}
+        <a
+          href="https://rocketseat.com.br"
+          className="hover:text-gray-100"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Rocketseat
+        </a>{' '}
+        por{' '}
+        <a
+          target="_blank"
+          rel="noreferrer noopener"
+          href="https://hemerson-dev.vercel.app/pt-BR"
+          className="hover:text-gray-100"
+        >
+          Hemerson Oliveira
+        </a>
+      </span>
+    </div>
+  )
+}
---
+++ NLW12-spacetime/web/src/components/EmptyMemories/index.tsx
@@ -0,0 +1,12 @@
+export function EmptyMemories() {
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <p className="max-w-[360px] text-center leading-relaxed">
+        Você ainda não registrou nenhuma lembrança, comece a{' '}
+        <a href="" className="underline transition-colors hover:text-gray-50">
+          criar agora
+        </a>
+      </p>
+    </div>
+  )
+}
